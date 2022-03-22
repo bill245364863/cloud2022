89961708ae3e7bc853ea99a4a50e31820052140e
huangxiaotao <245364863@qq.com>
2022-03-22 23:35:41 +0800

    提交hystrix

---
+++ cloud-provider-hygtrix-payment8001/src/main/java/com/bill/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,20 @@
+package com.bill.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @title: PaymentHystrixMain8001
+ * @Author huangxiaotao
+ * @Date:2022 03 2022/3/22 23:31
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
